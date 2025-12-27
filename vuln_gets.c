24773596a3464ca074b9f94e5d63ff1522a1bc7f
hoangduy0308 <hoangduyntt04@gmail.com>
2025-12-27 21:30:05 +0700

    abc

--- vuln_gets.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char password[8];
-    gets(password);
-    printf("Password: %s\n", password);
-    return 0;
-}
---
+++ vuln_overflow.c
@@ -0,0 +1,4 @@
+void copy_input(char *src) {
+    char buf[16];
+    strcpy(buf, src);
+}
